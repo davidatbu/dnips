17c95d5fa93f90291025515368eff7fe9f09f622
davidatbu <davidat@bu.edu>
2020-11-11 20:53:50 -0500

    added BiDict

---
+++ dnips/bidict.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+from functools import cached_property
+
+from typing import (
+    Dict,
+    Sequence,
+    Union,
+    Mapping,
+    MutableMapping,
+    overload,
+    Generic,
+    TypeVar,
+    Iterator,
+    Iterable,
+    Tuple,
+    AbstractSet,
+    ItemsView,
+    KeysView,
+    ValuesView,
+)
+
+_K = TypeVar("_K")
+_V = TypeVar("_V")
+
+_SENTINEL = object()
+
+
+class BiDict(MutableMapping[_K, _V]):
+    @overload
+    def __init__(self, __tuples: Iterable[Tuple[_K, _V]]) -> None:
+        ...
+
+    @overload
+    def __init__(self, __dict: Dict[_K, _V], rev_dict: Dict[_V, _K] = None) -> None:
+        ...
+
+    def __init__(
+        self,
+        __item: Union[Iterable[Tuple[_K, _V]], Dict[_K, _V]],
+        rev_dict: Dict[_V, _K] = None,
+    ) -> None:
+        if isinstance(__item, dict):
+            self._dict = dict(__item)
+        else:
+            self._dict = {k: v for k, v in __item}
+
+        if rev_dict is None:
+            self._rev_dict = {v: k for k, v in self._dict.items()}
+        else:
+            self._rev_dict = dict(rev_dict)
+
+    @cached_property
+    def rev(self) -> BiDict[_V, _K]:
+        return BiDict(self._rev_dict, self._dict)
+
+    def __len__(self) -> int:
+        return len(self._dict)
+
+    def __getitem__(self, k: _K) -> _V:
+        return self._dict[k]
+
+    def __delitem__(self, k: _K) -> None:
+        v = self._dict[k]
+        del self._dict[k]
+        del self._rev_dict[v]
+
+    def __setitem__(self, k: _K, v: _V) -> None:
+        self._dict[k] = v
+        self._rev_dict[v] = k
+
+    def items(self) -> ItemsView[_K, _V]:
+        return self._dict.items()
+
+    def keys(self) -> KeysView[_K]:
+        return self._dict.keys()
+
+    def values(self) -> ValuesView[_V]:
+        return self._dict.values()
+
+    def __iter__(self) -> Iterator[_K]:
+        return iter(self._dict.keys())
+
+
+_Tco = TypeVar("_Tco", covariant=True)
+
+
+class Ordering(Sequence[_Tco]):
+    def __init__(self, __item: Iterable[_Tco]) -> None:
+        self._bidict = BiDict(enumerate(__item))
+
+    @cached_property
+    def indices(self) -> Mapping[_Tco, int]:
+        return self._bidict.rev
+
+    @overload
+    def __getitem__(self, __item: int) -> _Tco:
+        ...
+
+    @overload
+    def __getitem__(self, __item: slice) -> Ordering[_Tco]:
+        ...
+
+    def __getitem__(self, __item: Union[int, slice]) -> Union[_Tco, Ordering[_Tco]]:
+        if isinstance(__item, int):
+            try:
+                return self._bidict[__item]
+            except KeyError: # Be a true collection
+                raise IndexError
+        else:
+            return Ordering(
+                self[i] for i in range(__item.start, __item.stop, __item.step)
+            )
+
+    def __len__(self) -> int:
+        return len(self._bidict)
+
+    def __contains__(self, __item: object) -> bool:
+        return __item in self._bidict.rev
+
+
+if __name__ == "__main__":
+    BiDict({})
+    Ordering([])
---
+++ tests/test_bidict.py
@@ -0,0 +1,26 @@
+
+from dnips.bidict import BiDict, Ordering
+
+def test_bidict() -> None:
+    bidict = BiDict(enumerate('abcdefg', 1))
+
+    assert list(bidict.keys()) == list(bidict.rev.values())
+
+
+    assert 'a' in bidict.rev
+    del bidict[1]
+    assert  'a' not in bidict.rev
+
+    bidict[1] = "A"
+    assert bidict[1] == "A"
+    assert bidict.rev["A"] == 1
+
+def test_ordering() -> None:
+    ordering = Ordering('abcdefg')
+
+    assert ordering[0] == 'a'
+
+
+    assert ordering.indices['a'] == 0
+
+    assert 'abcdefg' == ''.join(i for i in ordering) 
